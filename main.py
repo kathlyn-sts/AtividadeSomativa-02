c6479144df23b0b51b52cda320e720c7394ac52e
kathlyn-sts <kathlyn.santos1@pucpr.edu.br>
2024-09-23 09:35:46 -0300

    desenvolvimento02

--- main.py
+++ main.py
@@ -10,3 +10,16 @@ while True:
         break
     except ValueError:
         print("Oops!  That was no valid number.  Try again...")
+               
+            
+if ope in ['+']:
+    print(num1+num2)
+    
+if ope in ['-']:
+    print(num1-num2)
+
+if ope in ['*']:
+    print(num1*num2)
+
+if ope in ['/']:
+    print(num1/num2)
