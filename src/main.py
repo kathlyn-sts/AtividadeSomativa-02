1824649e235088a42d07f99cd653f63e915dd27b
kathlyn-sts <kathlyn.santos1@pucpr.edu.br>
2024-09-25 10:36:23 -0300

    update.test01

--- src/main.py
+++ src/main.py
@@ -1,25 +1,36 @@
-x = input('Digite seu nome:  ')
-print('Bem-vindo à Calculadora, ' + x)
+def adicionar(num1, num2):
+    return num1 + num2
 
-ope = input('Insira a operação matemática desejada (+, -, * or /): ')
+def subtrair(num1, num2):
+    return num1 - num2
 
-while True:
-    try:
-        num1 = float(input('Insira o primeiro número da operação: '))
-        num2 = float(input('Insira o segundo número da operação: '))
-        break
-    except ValueError:
-        print("Oops!  Esse não era um número válido.   Tente novamente...")
-               
-            
-if ope in ['+']:
-    print(num1+num2)
+def multiplicar(num1, num2):
+    return num1 * num2
+
+def dividir(num1, num2):
+    if num2 == 0:
+        raise ValueError("Divisão por zero não é permitida.")
+    return num1 / num2
+
+def calculadora():
+    nome = input('Digite seu nome: ')
+    print(f'Bem-vindo à Calculadora, {nome}')
+
+    operacao = input('Insira a operação matemática desejada (+, -, * or /): ')
     
-if ope in ['-']:
-    print(num1-num2)
+    while True:
+        try:
+            num1 = float(input('Insira o primeiro número da operação: '))
+            num2 = float(input('Insira o segundo número da operação: '))
+            break
+        except ValueError:
+            print("Oops! Esse não era um número válido. Tente novamente...")
 
-if ope in ['*']:
-    print(num1*num2)
-
-if ope in ['/']:
-    print(num1/num2)
+    if operacao == '+':
+        print(adicionar(num1, num2))
+    elif operacao == '-':
+        print(subtrair(num1, num2))
+    elif operacao == '*':
+        print(multiplicar(num1, num2))
+    elif operacao == '/':
+        print(dividir(num1, num2))
