6e148c77cfc2aa0b9c6be75c73767ce95f9e4be9
kathlyn-sts <kathlyn.santos1@pucpr.edu.br>
2024-09-24 21:29:46 -0300

    update.main

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ ope = input('Insira a operação matemática desejada (+, -, * or /): ')
 while True:
     try:
         num1 = float(input('Insira o primeiro número da operação: '))
-        num2 = float(input('Insira o segundo número da operação:   '))
+        num2 = float(input('Insira o segundo número da operação: '))
         break
     except ValueError:
-        print("Oops!  Esse não era um número válido.  Tente novamente...  ")
+        print("Oops!  Esse não era um número válido.  Tente novamente...")
                
             
 if ope in ['+']:
